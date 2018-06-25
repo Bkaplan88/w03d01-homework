528b2f07899c0979855ff8424936e3d7501371f2
Benjamin Kaplan <benjaminkaplanart@gmail.com>
2018-06-25 15:14:37 -0600

    instantiated class

---
+++ js/app.js
@@ -0,0 +1,17 @@
+let name = prompt("What is your Tamogatchi's name?");
+class Tamogatchi {
+	constructor(name, age, hunger, entertained, energy, ) {
+		this.name = name;
+		this.age = age;
+		this.hunger = hunger;
+		this.entertained = entertained;
+		this.energy = energy;
+
+
+	}
+
+};
+
+const firstTamogatchi = new Tamogatchi(name, 1, 10, 10, 10);
+
+console.log(firstTamogatchi);
